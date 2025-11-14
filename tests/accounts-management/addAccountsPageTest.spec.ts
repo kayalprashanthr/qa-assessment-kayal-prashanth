a911dd2fadd14078e932ef9a569f879c1ee6abfd
Kayal Prashanth R <kayalprashanth.r@gmail.com>
2025-11-14 09:20:57 +0530

    Update account keys and secrets in tests

--- tests/accounts-management/addAccountsPageTest.spec.ts
+++ tests/accounts-management/addAccountsPageTest.spec.ts
@@ -15,15 +15,15 @@ test("addAccounts",async({page})=>{
     const Password:string = "Ethishan03$";
     let OKXName:string = "AutomationTestOKX";
     const OKXKey:string = "8aa95d56-a96c-4df1-b8d6-791ee57d1b58";
-    const OKXSecret:string = "EB067275097F317095DDAC7F89CBBDB0";
+    const OKXSecret:string = "097F317095DDAC7F89CBBDB0";
     // Binance USD-M
     let USDMName:string = "AutomationTestUSDM";
-    const USDMKey:string = "fnSrifSofyjR24ybGLIkXm5ETuy9ftDqGPObdUOoxmoKY9dd4TCJrnnNgPxcHt23";
-    const USDMSecret:string = "4hBoEvE1SHgfR9GTmF0df7y4wKfqTTET2B2MOmMzWoFTXwz1d8rbDvEAVGhboKkJ";
+    const USDMKey:string = "fnSrifSofyjR24ybGLIkXmETuy9ftDqGPObdUOoxmoKY9dd4TCJrnnNgPxcHt23";
+    const USDMSecret:string = "4F0df7y4wKfqTTET2B2MOmMzWoFTXwz1d8rbDvEAVGhboKkJ";
     // Binance Coin-M
     let CoinMName:string = "AutomationTestCoinM";
-    const CoinMKey:string = "fnSrifSofyjR24ybGLIkXm5ETuy9ftDqGPObdUOoxmoKY9dd4TCJrnnNgPxcHt23";
-    const CoinMSecret:string = "4hBoEvE1SHgfR9GTmF0df7y4wKfqTTET2B2MOmMzWoFTXwz1d8rbDvEAVGhboKkJ";
+    const CoinMKey:string = "fnSrifSofyjR24ybGLIkXm5ETuy9fDqGPObdUOoxmoKY9dd4TCJrnnNgPxcHt23";
+    const CoinMSecret:string = "4h7y4wKfqTTET2B2MOmMzWoFTXwz1d8rbDvEAVGhboKkJ";
     const acc = new addAccounts(page);
     await acc.addAccOKX(Password, OKXName, OKXKey, OKXSecret);
     await acc.addAccUSDM(USDMName, USDMKey, USDMSecret);
